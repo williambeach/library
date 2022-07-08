3fec347b65ca480103ae625d330bdc7f245a7a36
williambeach <intratech.cs@icloud.com>
2022-07-07 23:51:41 -0400

    create search function for title/author

--- javascript.js
+++ javascript.js
@@ -1,16 +1,17 @@
 let myLibrary = [{title: "The Principles of Object-Oriented JavaScript", author: "Nicholas C. Zakas",
 pages: 120, read: true}, {title: "Inside the Machine: An Illustrated Introduction to Microprocessors and Computer Architecture", author: "Jon Stokes", 
-pages: 320, read: false}, {title: "Structure and Interpretation of Computer Programs", author: ["Harold Abelson", "Gerald Jay Sussman", "Julie Sussman"],
-pages: 657, read: false}, {title: "Design Patterns: Elements of Reusable Object-Oriented Software", author: ["Erich Gamma", "Richard Helm", "Ralph Johnson", "John Vlissides"],
+pages: 320, read: false}, {title: "Structure and Interpretation of Computer Programs", author: "Harold Abelson, Gerald Jay, Sussman Julie Sussman",
+pages: 657, read: false}, {title: "Design Patterns: Elements of Reusable Object-Oriented Software", author: "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides",
 pages: 416, read: false}, {title: "Code: The Hidden Language of Computer Hardware and Software", author: "Charles Petzold",
 pages: 400, read: false}, {title: "Cracking the Coding Interview: 189 Programming Questions and Solutions", author: "Gayle Laakmann McDowell",
 pages: 687, read: true}, {title: "Code Complete: A Practical Handbook of Software Construction", author: "Steve McConnell", 
 pages: 960, read: false}, {title: "Programming Pearls", author: "Jon Bentley",
-pages: 256, read: false}, {title: "The Pragmatic Programmer: Your Journey to Mastery", author: ["Andrew Hunt", "David Thomas"],
+pages: 256, read: false}, {title: "The Pragmatic Programmer: Your Journey to Mastery", author: "Andrew Hunt, David Thomas",
 pages: 352, read: false}, {title: "Code Simplicity", author: "Max Kanat-Alexander",
-pages: 84, read: false}, {title: "Algorithms to Live By: The Computer Science of Human Decisions", author: "Jon Bentley",
+pages: 84, read: false}, {title: "Algorithms to Live By: The Computer Science of Human Decisions", author: "Brian Christian Tom Griffiths",
 pages: 368, read: false}, {title: "Clean Code: A Handbook of Agile Software Craftmanship", author: "Robert C. Martin",
-pages: 464, read: false}];
+pages: 464, read: false}, {title: "Think Like a Programmer: An Introduction to Creative Problem Solving", author: "V. Anton Spraul",
+pages: 256, read: false}];
 
 const buttons = document.querySelectorAll('button');
 const books = document.querySelectorAll('.book');
@@ -63,9 +64,26 @@ function searchBooks() {
     }
 }
 
+function search() {
+    for (let i=0; i<myLibrary.length;i++) {
+        searchBar.value = searchBar.value.toLowerCase();
+        if (myLibrary[i].author.toLowerCase().includes(searchBar.value) || myLibrary[i].title.toLowerCase().includes(searchBar.value)) {
+            console.log(myLibrary[i]);
+        }
+    }
+}
 
+function searchAlgo() {
+    searchBarButton.addEventListener('click', ()=> {
+        searchButton.value = "off";
+        search();
+        newDiv.remove();
+        body.style.gridTemplateColumns = "60px 1fr";
+    });
+}
 
 
 
 hoverEffect();
 searchBooks();
+searchAlgo();
