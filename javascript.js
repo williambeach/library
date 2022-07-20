0f64cc3f7174a0593a0ac1cb001deae5f3b9e1fb
williambeach <intratech.cs@icloud.com>
2022-07-19 20:55:25 -0400

    add hover effect to books after deletion

--- javascript.js
+++ javascript.js
@@ -530,6 +530,7 @@ function removeBook() {
            removeIcons();
            deleteBooks();
            showBooks();
+           hoverEffect();
            deleteBookButton.value = "off";
         });
     }
